73f0567f4f53ac8ea5ec906fe29d406f190fe0a4
kute <kutekute00@gmail.com>
2016-09-16 17:18:36 +0800

    email util

---
+++ src/main/java/com/kute/util/CommonEmailUtil.java
@@ -0,0 +1,74 @@
+package com.kute.util;
+
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.MultiPartEmail;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Created by kute on 16/9/16.
+ */
+public class CommonEmailUtil {
+
+    private static String username;
+    private static String password;
+    private static String host;
+
+    private CommonEmailUtil() {
+    }
+
+    static {
+        ResourceBundle rb = ResourceBundle.getBundle("mail", Locale.ENGLISH);
+
+        //获取配置参数
+        host = rb.getString("mail.smtp.host");
+        username = rb.getString("username");
+        password = rb.getString("password");
+    }
+
+    /**
+     *
+     * @param files
+     * @param subject
+     * @param htmlContent
+     * @param recieves
+     */
+    public static void sendMail(File[] files, String subject, String htmlContent, String[] recieves) {
+        // SimpleEmail email = new SimpleEmail();
+        MultiPartEmail email = new MultiPartEmail();
+        // HtmlEmail email = new HtmlEmail();
+
+        email.setHostName(host);
+        email.setAuthentication(username, password);
+        email.setCharset("UTF-8");
+        try {
+            email.setFrom(username);
+            email.setSubject(subject);
+//            email.setMsg(htmlContent);
+            email.addPart(htmlContent, "text/html; charset=UTF-8");
+
+            if (null != files && files.length > 0) {
+                for (File file : files) {
+                    EmailAttachment attachment = new EmailAttachment();
+                    attachment.setPath(file.getPath());
+                    attachment.setName(file.getName());
+                    attachment.setDescription(subject);
+                    attachment.setDisposition(EmailAttachment.ATTACHMENT);
+                    email.attach(attachment);
+                }
+            }
+            if (null != recieves && recieves.length > 0) {
+                for (String to : recieves) {
+                    email.addTo(to);
+                }
+                email.send();
+            }
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/kute/util/SimpleEmailUtil.java
@@ -0,0 +1,100 @@
+package com.kute.util;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+/**
+ * Created by kute on 16/9/16.
+ */
+public class SimpleEmailUtil {
+
+
+    private static String username;
+    private static String password;
+    private static String host;
+    private static String port;
+
+    private static  Properties props = new Properties();
+
+
+    static {
+        ResourceBundle rb = ResourceBundle.getBundle("mail", Locale.ENGLISH);
+
+        //获取配置参数
+        host = rb.getString("mail.smtp.host");
+        port = rb.getString("mail.smtp.port");
+        username = rb.getString("username");
+        password = rb.getString("password");
+
+        //设置参数
+        props = new Properties();
+        props.setProperty("mail.smtp.auth", rb.getString("mail.smtp.auth"));
+        props.setProperty("mail.smtp.starttls.enable",rb.getString("mail.smtp.starttls.enable"));
+        props.setProperty("mail.smtp.host", host);
+        props.setProperty("mail.smtp.port", port);
+        props.setProperty("mail.smtp.ssl.enable", "true");
+        props.setProperty("mail.smtp.ssl.trust", host);
+        props.setProperty("mail.smtp.ssl.checkserveridentity","false");
+    }
+
+    public static boolean sendEmail() throws Exception {
+
+        //获取session对象
+        Session session = Session.getInstance(props,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        //打印debug信息
+        session.setDebug(true);
+
+        //邮件内容
+        Message msg = new MimeMessage(session);
+
+        msg.setFrom(new InternetAddress(username));
+
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress("solo.he@mpos.net"));
+
+        msg.setSentDate(new Date());
+
+        msg.setSubject("hava a test");
+
+        MimeBodyPart mbp = new MimeBodyPart();
+        mbp.setContent("</br>hava a test</br>hava a test</br>", "text/html;charset=UTF-8");
+
+        Multipart mulp = new MimeMultipart();
+        mulp.addBodyPart(mbp);
+
+        msg.setContent(mulp);
+
+        //获取Service对象
+        Transport tran = session.getTransport("smtp");
+
+        //连接服务器
+        tran.connect(host, username, password);
+
+        //发送邮件
+        tran.sendMessage(msg, msg.getAllRecipients());
+
+        return true;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        sendEmail();
+    }
+}

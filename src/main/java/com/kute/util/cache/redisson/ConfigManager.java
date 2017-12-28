94c7dcabc3f65997976a735a4489e07637da88b0
longbai <longbai@corp.netease.com>
2017-12-28 09:38:09 +0800

    add redisson

---
+++ src/main/java/com/kute/util/cache/redisson/ConfigManager.java
@@ -0,0 +1,38 @@
+package com.kute.util.cache.redisson;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.redisson.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.base.Joiner;
+
+/**
+ * Created by longbai on 2017年12月27日下午6:03:27
+ **/
+public class ConfigManager {
+    
+    private static final Logger logger = LoggerFactory.getLogger(ConfigManager.class);
+
+    public static Config getConfig(String fileLocation) {
+        Config config = null;
+        try {
+            config = Config.fromJSON(new File(fileLocation));
+        } catch (IOException e) {
+            config = new Config();
+            logger.error(Joiner.on(" ").join("Get config from parsing file[", fileLocation, "] error and use default config"), e);
+        }
+        return config;
+    }
+    
+    public static JSONObject getConfig(Config config) throws IOException {
+        if(null != config) {
+            return JSONObject.parseObject(config.toJSON());
+        }
+        return null;
+    }
+    
+}
---
+++ src/main/java/com/kute/util/cache/redisson/RedissonUtil.java
@@ -0,0 +1,16 @@
+package com.kute.util.cache.redisson;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.redisson.config.Config;
+
+/**
+ * Created by longbai on 2017年12月27日下午5:56:29
+ *
+ **/
+public class RedissonUtil {
+    
+    private static final String CONFIG_FILE = "redisson-config.json";
+    
+}

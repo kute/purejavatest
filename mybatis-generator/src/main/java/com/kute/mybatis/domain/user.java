7e20e4f18dac73978558a83a4a912282bc80223f
bailong03 <bailong03@meituan.com>
2018-02-08 12:58:01 +0800

    mybatis-generator

---
+++ mybatis-generator/src/main/java/com/kute/mybatis/domain/user.java
@@ -0,0 +1,35 @@
+package com.kute.mybatis.domain;
+
+import java.util.Date;
+
+public class user {
+    private Long id;
+
+    private String name;
+
+    private Date birthday;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
---
+++ src/main/java/com/kute/mybatis/mapper/userMapper.java
@@ -0,0 +1,17 @@
+package com.kute.mybatis.mapper;
+
+import com.kute.mybatis.domain.user;
+
+public interface userMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(user record);
+
+    int insertSelective(user record);
+
+    user selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(user record);
+
+    int updateByPrimaryKey(user record);
+}

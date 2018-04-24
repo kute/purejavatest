cc4a56e76bd95da0ebd2512e5d73131751a988cb
kute <kutekute00@gmail.com>
2018-04-24 23:11:17 +0800

    guava-retrying

--- src/main/java/com/kute/guava/retry/RetryTest.java
+++ src/main/java/com/kute/guava/retry/RetryTest.java
@@ -1,9 +1,6 @@
 package com.kute.guava.retry;
 
-import com.github.rholder.retry.RetryException;
-import com.github.rholder.retry.Retryer;
-import com.github.rholder.retry.RetryerBuilder;
-import com.github.rholder.retry.StopStrategies;
+import com.github.rholder.retry.*;
 import com.google.common.base.Predicates;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -12,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -35,8 +33,25 @@ public class RetryTest {
                 .retryIfResult(Predicates.isNull())
                 .retryIfExceptionOfType(IOException.class)
                 .retryIfRuntimeException()
-                .withStopStrategy(StopStrategies.stopAfterAttempt(3))
+                // 重试等待时间(ms) = 100 * (2 ^ 重试次数)，指数级增长方式，最多等待 5s
+                .withWaitStrategy(WaitStrategies.exponentialWait(100, 5, TimeUnit.SECONDS))
+                // 重试等待时间=斐波那契数列 增长方式
+//                .withWaitStrategy(WaitStrategies.fibonacciWait(100, 5, TimeUnit.SECONDS))
+                .withStopStrategy(StopStrategies.neverStop())
+//                .withStopStrategy(StopStrategies.stopAfterAttempt(3))
 //                .withStopStrategy(StopStrategies.stopAfterDelay(3, TimeUnit.MINUTES))
+                // 休眠等待的方式，默认是 Thread.sleep
+                .withBlockStrategy(BlockStrategies.threadSleepStrategy())
+                .withRetryListener(new RetryListener() {
+                    @Override
+                    public <V> void onRetry(Attempt<V> attempt) {
+                        try {
+                            logger.info("{}={}={}={}", attempt.getAttemptNumber(), attempt.get(), attempt.hasResult(), attempt.hasException());
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                })
                 .build();
 
         try {
